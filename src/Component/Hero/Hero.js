8eec455ba0c13caa3ea6bb903cacf3adb69f4981
coderrabbie <golamrabbi.dv@gmail.com>
2022-09-05 00:32:41 +0600

    done

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React } from "react";
 import "./Hero.css";
 import Typical from "react-typical";
 import { Button, Col, Container, Row } from "react-bootstrap";
